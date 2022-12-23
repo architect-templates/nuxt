5b24510157bbd5bc0ec08c0de1bbf5dffb55040e
Ryan Cahill <ryan.cahill444@gmail.com>
2022-12-22 21:04:43 -0500

    updating favicon and small things

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,7 +17,7 @@ const config = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/png', href: '/architect-logo.png' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
@@ -50,7 +50,6 @@ const config = {
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
-  // // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     defaultAssets: true,
     treeShake: true,
