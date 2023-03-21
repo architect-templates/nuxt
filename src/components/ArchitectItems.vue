22fa5cad5357dc0cfe9955c628cf0f4ee6a06ee9
Michael Muesch <michael.muesch@architect.io>
2023-03-21 16:38:33 -0400

    Updated to new standards

--- src/components/ArchitectItems.vue
+++ src/components/ArchitectItems.vue
@@ -1,39 +1,34 @@
 <template>
   <v-container>
-    <h2>Favorite movies</h2>
-    <div class="user_inputs">
-      <label
-        class="form-label">
-        Movie Title
-      </label>
-      <input
-        placeholder="Movie Title"
-        type="text"
-        class="form-control"
-        maxlength="80"
-        v-model="movie_name"
-      >
-    </div>
-    <div class="user_inputs">
-      <label
-        class="form-label">
-        Rating
-      </label>
-      <input
-        placeholder="Rating 1-5"
-        type="number"
-        class="form-control"
-        value=""
-        min="1"
-        max="5"
-        v-model="movie_rating"
-      >
-    </div>
-    <div class="d-grid gap-2">
-      <button @click="sendData">
-        Submit
-      </button>
-    </div>
+    <v-row justify="center">
+      <v-col cols="3">
+        <input
+          placeholder="Movie Title"
+          type="text"
+          class="form-control"
+          maxlength="80"
+          v-model="movie_name"
+        />
+      </v-col>
+      <v-col cols="3">
+        <input
+          placeholder="Rating 1-5"
+          type="number"
+          class="form-control"
+          value=""
+          min="1"
+          max="5"
+          v-model="movie_rating"
+        />
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="4">
+        <button @click="sendData">
+          Submit
+        </button>
+      </v-col>
+    </v-row>
 
     <div class="mt-4">
       <p v-if="loading_movies">Loading....</p>
